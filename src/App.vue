102c90d0ce1899c3e5665990d0c1bc7636979ae8
hzc19970630 <1034181523@qq.com>
2018-10-27 12:28:58 +0800

    two-commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <leftNav></leftNav>
+    <div class="main">
+      <router-view/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'App',
+  components:{
+    leftNav,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Microsoft YaHei', 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  height: 100%;
+}
+
+.main {
+  margin-left:80px;
+  background-color: #EFF2F7;
+  height: 100%;
+  overflow: auto;
+}
+
+.el-tabs__content {
+  padding: 0 10px;
+}
+.el-table .cell, .el-table th>div {
+    padding-left: 10px;
+    padding-right: 10px;
+}
+</style>
